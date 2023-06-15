44ba62ca5a05d3446f0c5082885fa2abf1abece6
Alfredo Valle <phredvalle@gmail.com>
2023-06-15 00:36:53 -0500

    Add basic styling and google fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './default.scss';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hellow e-Comm world!</h2>
+      <Header />
+      <h2>Hello e-Comm world!</h2>
     </div>
   );
 }
